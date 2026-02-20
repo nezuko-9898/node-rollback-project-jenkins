6b0978cb3b5a6a9ab4b9b34925c99d9b21aa85e9
Mali Pravin <malipravin5545@gmail.com>
2026-02-20 16:34:02 +0530

    new commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
-    res.send("Node App Running Successfully")
+    res.send("Node App Running Successfully hiii hell")
 })
 
 app.listen(3000, () => {

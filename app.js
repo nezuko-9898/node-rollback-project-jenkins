51117d7e4a53803a6b14df4db9d1510577aad46f
Mali Pravin <malipravin5545@gmail.com>
2026-02-20 17:29:26 +0530

    neww error commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+// 🔥 Intentional crash
+throw new Error("ROLLBACK TEST ERROR")
+
 app.get('/', (req,res)=>{
     res.send("This should never show")
 })
